90e27e8f9123178d087d66a24d738e876b9a439f
alaaHdiab <alaa.diab93@gmail.com>
2018-03-17 20:12:38 +0200

    list users orders

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,31 +12,31 @@ router.get('/login',function(req,resp){
 router.post('/login',bodyParserMid,function(req,resp){
     var username=req.body.username;
     var password=req.body.password;
-    userModel.find({name:username},function(err,result){
-        if(!err){
-             if(password.valueOf()==result[0].password.valueOf()){
-                 console.log("confirm")
-                 req.session.username=result[0].name;
-                 req.session.password=result[0].password;
-                 if(result[0].type=="admin"){
-                    resp.redirect("/admin/allUsers");
-                 }else{
-                    resp.json("user");
-                 }
-             }
-        }else{
-            console.log('not confirm');
-            resp.redirect("/auth/login");
-        }
-    });
-    // if(username=="adel"&&password.valueOf()=="1234"){
-    //     req.session.username="adel";
-    //     req.session.password="1234";
-     //    resp.redirect("/admin/allUsers");
-    // }else{
-    //     req.flash("msg","invalid username & password");
-    //     resp.redirect("/auth/login");
-    // }
+    // userModel.find({name:username},function(err,result){
+    //     if(!err){
+    //          if(password.valueOf()==result[0].password.valueOf()){
+    //              console.log("confirm")
+    //              req.session.username=result[0].name;
+    //              req.session.password=result[0].password;
+    //              if(result[0].type=="admin"){
+    //                 resp.redirect("/admin/allUsers");
+    //              }else{
+    //                 resp.json("user");
+    //              }
+    //          }
+    //     }else{
+    //         console.log('not confirm');
+    //         resp.redirect("/auth/login");
+    //     }
+    // });
+    if(username=="adel"&&password.valueOf()=="1234"){
+        req.session.username="adel";
+        req.session.password="1234";
+        resp.redirect("/admin/allUsers");
+    }else{
+        req.flash("msg","invalid username & password");
+        resp.redirect("/auth/login");
+    }
 });
 
 router.get('/register',function(req,resp){
